16a906397aad8fd72101b59987bb47d04fe2b520
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-08-24 10:46:50 +0300

    Commit

--- BlogApp.Web/Program.cs
+++ BlogApp.Web/Program.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Reflection;
 using BlogApp.Data.Context;
+using BlogApp.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
