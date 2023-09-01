6cba0413b7d5af465fb328478f2aa0087d6ef814
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-02 02:52:54 +0300

    Commit

---
+++ BlogApp.Web/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using BlogApp.Entity.DTOs.Users;
+using BlogApp.Entity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(userLoginDto.Email);
+                if(user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, userLoginDto.Password, userLoginDto.RememberMe, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home", new {Area = "Admin"});
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "E-Mail or Password is not correct!");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "E-Mail or Password is not correct!");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
